1153919830a81b7bb45d44b6b1fdffd82fc2afa8
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-11-30 18:13:46 +0100

    Result of session 1.

--- POS/POS.Tests/Test.cs
+++ POS/POS.Tests/Test.cs
@@ -1,15 +1,161 @@
 ﻿
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
 using NUnit.Framework;
 
 namespace POS.Tests
 {
     [TestFixture]
-    class Test
+    internal class Test
     {
-        [Test]
-        public void Fail()
+        Register uut;
+        private TextWriter textWriter;
+
+        [SetUp]
+        public void Setup()
         {
-            Assert.Fail();
+            textWriter = new StringWriter();
+            uut = new Register(textWriter);
+        }
+
+        [Test]
+        public void SuccessfulInput()
+        {
+            uut.Add("1", 100);
+            Assert.AreEqual("1,00 €", uut.Scan("1"));
+        }
+
+        [Test]
+        public void UnsuccessfulInput()
+        {
+            Assert.AreEqual("Barcode not found : \"fail\"", uut.Scan("fail"));
+        }
+
+        [Test]
+        public void BarcodeReadError()
+        {
+            Assert.AreEqual("Read Failure",uut.Scan(""));
+        }
+
+        [Test]
+        public void LessThanOneEuro()
+        {
+            uut.Add("50003", 2);
+            Assert.AreEqual("0,02 €", uut.Scan("50003"));
+        }
+
+        [Test]
+        public void MultipleItems()
+        {
+            uut.Add("2056", 2000);
+            uut.Add("50003", 2);
+            uut.Add("0055", 150);
+
+            Assert.AreEqual("20,00 €", uut.Scan("2056"));
+            Assert.AreEqual("0,02 €", uut.Scan("50003"));
+            Assert.AreEqual("1,50 €", uut.Scan("0055"));
+        }
+
+        [Test]
+        public void TotalAmount()
+        {
+            uut.Add("2056", 2000);
+            uut.Scan("2056");
+            Assert.AreEqual("20,00 €", uut.Total());
+        }
+
+        [Test]
+        public void MultipleScans()
+        {
+            uut.Add("2056", 2000);
+            uut.Add("0055", 150);
+            uut.Scan("2056");
+            uut.Scan("0055");
+            uut.Scan("0055");
+            Assert.AreEqual("23,00 €", uut.Total());
+        }
+
+        [Test]
+        public void ResetAfterTotal()
+        {
+            uut.Add("2056", 2000);
+            uut.Scan("2056");
+            Assert.AreEqual("20,00 €", uut.Total());
+            Assert.AreEqual("0,00 €", uut.Total());
+        }
+
+        [Test]
+        public void MultipleTotals()
+        {
+            uut.Add("2056", 2000);
+            uut.Scan("2056");
+            Assert.AreEqual("20,00 €", uut.Total());
+            uut.Scan("2056");
+            uut.Scan("2056");
+            Assert.AreEqual("40,00 €", uut.Total());
+        }
+
+        [Test]
+        public void MultipleScansWithFailure()
+        {
+            uut.Add("2056", 2000);
+            uut.Add("0055", 150);
+            uut.Scan("2056");
+            uut.Scan("0055");
+            uut.Scan("");
+            uut.Scan("1337");
+            uut.Scan("0055");
+            Assert.AreEqual("23,00 €", uut.Total());
+        }
+
+        [Test]
+        public void LiveOutputOnScan()
+        {
+            uut.Add("2056", 2000);
+            uut.Scan("2056");
+            Assert.AreEqual("20,00 €" + Environment.NewLine, textWriter.ToString());
+        }
+
+        [Test]
+        public void LiveOutputOnTotal()
+        {
+            uut.Total();
+            Assert.AreEqual("Total: 0,00 €" + Environment.NewLine
+                , textWriter.ToString());
+        }
+
+        [Test]
+        public void LiveOutputOnScanFailure()
+        {
+            uut.Scan("");
+            Assert.AreEqual("Read Failure" + Environment.NewLine, textWriter.ToString());
+        }
+
+        [Test]
+        public void LiveOutputOnBarcodeNotFound()
+        {
+            uut.Scan("uiae");
+            Assert.AreEqual("Barcode not found : \"uiae\"" + Environment.NewLine, textWriter.ToString());
+        }
+
+        [Test]
+        public void LiveOutputWithMultipleScansAndTotal()
+        {
+            uut.Add("2056", 2000);
+            uut.Scan("2056");
+            uut.Scan("2056");
+            uut.Total();
+            uut.Scan("");
+            uut.Scan("qwerf");
+            var sb = new StringBuilder();
+            sb.AppendLine("20,00 €");
+            sb.AppendLine("20,00 €");
+            sb.AppendLine("Total: 40,00 €");
+            sb.AppendLine("Read Failure");
+            sb.AppendLine("Barcode not found : \"qwerf\"");
+            Assert.AreEqual(sb.ToString(), textWriter.ToString());
         }
     }
 }
---
+++ POS/Register.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS
+{
+    public class Register
+    {
+        private Dictionary<string, int> _priceMapping;
+        private int _totalPrice;
+        private TextWriter _textWriter;
+
+        public Register(TextWriter textWriter)
+        {
+            _priceMapping = new Dictionary<string, int>();
+            _totalPrice = 0;
+            this._textWriter = textWriter;
+        }
+      
+
+        public string Scan(string barCode)
+        {
+            var priceString = ScanInternal(barCode);
+            _textWriter.WriteLine(priceString);
+            return priceString;
+        }
+
+        private string ScanInternal(string barCode)
+        {
+            if (barCode.Equals(""))
+            {
+                return "Read Failure";
+            }
+            if (!_priceMapping.ContainsKey(barCode))
+            {
+                return "Barcode not found : \"" + barCode + "\"";
+            }
+            var cents = _priceMapping[barCode];
+            _totalPrice += cents;
+            var formatCurrency = FormatCurrency(cents);
+            return formatCurrency;
+        }
+
+        private static string FormatCurrency(int cents)
+        {
+            var price = cents/100.0;
+            var priceString = price.ToString("C2");
+            return priceString;
+        }
+
+        public void Add(string barCode, int price)
+        {
+            _priceMapping.Add(barCode, price);
+        }
+
+        public string Total()
+        {
+            var totalString = FormatCurrency(_totalPrice);
+            _totalPrice = 0;
+            _textWriter.WriteLine("Total: " + totalString);
+            return totalString;
+        }
+    }
+}
