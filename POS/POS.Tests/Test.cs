48bf1ab6f8ba2b3983fc96c456b2df28a1fe4fc0
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2015-11-30 12:48:14 +0100

    Setup solution for POS kata.

---
+++ POS/POS.Tests/Test.cs
@@ -0,0 +1,15 @@
+﻿
+using NUnit.Framework;
+
+namespace POS.Tests
+{
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void Fail()
+        {
+            Assert.Fail();
+        }
+    }
+}
